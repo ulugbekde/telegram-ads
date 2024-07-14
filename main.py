e3db32a23e02dc4b8656fd99953c04daf58aa946
Ulug'bek Bolqiboyev <bolqiboyevuz@gmail.com>
2024-07-13 22:43:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import telebot
+from telebot.types import *
 
+
+web_app_url = "https://exemple.com/"
+
+bot = telebot.TeleBot("API_KEY",parse_mode='html')
+
+
+
+
+@bot.message_handler(commands=['start'])
+def start_handler(msg):
+    bot.reply_to(msg,"<b>O'yini boshlash uchun bosing! 👇</b>",reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(text="Start",web_app=WebAppInfo(web_app_url))))
+
+
+
+
+print("Bot username: @"+bot.get_me().username)
+bot.infinity_polling()
